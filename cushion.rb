27e9e4d73a0819f0f3cd82d71954b148e5afa434
Michael Gorsuch <michael@styledbits.com>
2009-11-05 22:43:11 -0500

    cleaned to minimum

--- cushion.rb
+++ cushion.rb
@@ -1,49 +1,6 @@
 require 'rubygems'
 require 'sinatra'
 
-require 'models'
-require 'helpers'
-
-enable :sessions
-
 get "/" do
   erb :index
-end
-
-get "/register" do 
-  erb :register
-end
-
-post "/register" do
-  u = User.new
-  u.attributes = params
-  if !u.save 
-    u.errors.inspect
-  else
-      session[:user_id] = u.id
-      redirect '/'
-  end
-end
-
-get "/login" do
-  erb :login
-end
-
-post "/login" do
-  u = User.login(params[:email], (params[:password]))  
-  if u
-    session[:user_id] = u.id
-    redirect "/"
-  else
-    "nope"
-  end
-end
-
-get "/logout" do
-  session[:user_id] = nil
-  redirect "/"
-end
-
-get "/sits/new/?" do
-  erb :sit_new
 end
