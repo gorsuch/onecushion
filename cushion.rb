7f40603641e530cea502ad81c9496b711c1bc458
Michael Gorsuch <michael@styledbits.com>
2009-11-06 09:59:36 -0500

    Handling 500's: - added configure block to set bugzscout params - added default error block to send all exceptions to bugzscout - created a simple template to be displayed on error

--- cushion.rb
+++ cushion.rb
@@ -2,6 +2,27 @@ require 'rubygems'
 require 'sinatra'
 require 'bugzscout'
 
+configure do
+  # configure bugzscout essentials
+  set :bugzscout_url, ENV['BUGZSCOUT_URL'] || 'https://foobar.fogbugz.com/scoutsubmit.asp'
+  set :bugzscout_user, ENV['BUGZSCOUT_USER'] || 'John Doe'
+  set :bugzscout_project, ENV['BUGZSCOUT_PROJECT'] || 'Some Project'
+  set :bugzscout_area, ENV['BUGZSCOUT_AREA'] || 'Some Area'
+end
+
+error do
+  BugzScout.submit(options.bugzscout_url) do |scout|
+    scout.user = options.bugzscout_user
+    scout.project = options.bugzscout_project
+    scout.area = options.bugzscout_area
+    scout.title = env['sinatra.error'].class.name
+    scout.body = env['sinatra.error'].message + "\n\n" + env['sinatra.error'].backtrace.to_s
+  end
+  
+  erb :error_500
+  
+end
+
 get "/" do
   erb :index
 end
