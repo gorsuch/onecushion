db10b6494896a8bd04bf9f4ed29b96789a230298
Michael Gorsuch <michael@styledbits.com>
2009-11-07 22:35:22 -0500

    removed bugzscout helpers and what not since we're relying on Rack::BugzScout now

--- cushion.rb
+++ cushion.rb
@@ -7,28 +7,6 @@ configure do
   set :raise_errors, true
 end
 
-helpers do
-  def scoutsubmit(title, message)
-    begin 
-      BugzScout.submit(options.bugzscout_url) do |scout|
-        scout.user = options.bugzscout_user
-        scout.project = options.bugzscout_project
-        scout.area = options.bugzscout_area
-        scout.title = title
-        scout.body = message
-      end
-    rescue
-      return false
-    end
-  end
-end
-
-error do
-  # we protect this so we don't get caught in some odd error loop in the case that this fails
-  scoutsubmit(env['sinatra.error'].class.name, env['sinatra.error'].message + "\n\n" + env['sinatra.error'].backtrace.to_s)  
-  erb :error_500
-end
-
 get "/" do
   erb :index
 end
