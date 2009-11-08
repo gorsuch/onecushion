e111ac32b4901f85d6f09cfa8117a421720df8f6
= <michael@styledbits.com>
2009-11-08 01:12:52 -0500

    only set raise_errors to true if we're in prod or staging mode

--- cushion.rb
+++ cushion.rb
@@ -3,7 +3,7 @@ require 'sinatra'
 
 configure do
   # start raising errors beyond this application
-  set :raise_errors, true
+  set(:raise_errors, true) if ENV['RACK_ENV'] != 'development'
 end
 
 get "/" do
