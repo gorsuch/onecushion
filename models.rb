dd667f0633a4ad2594d049da66fa40bbc451e5b9
= <michael@styledbits.com>
2009-11-11 22:29:32 -0500

    added missing Tweet properties to match up with Twitter's schema, and created a class 'fetch' method to pickup our tweets

--- models.rb
+++ models.rb
@@ -11,4 +11,24 @@ class Tweet
   property :from_user, String
   property :profile_image_url, String
   property :text, String
+  property :created_at, DateTime
+  property :to_user_id, Integer
+  property :geo, String #unsure about this data type
+  property :iso_language_code, String
+  property :source, String
+  
+  def self.fetch
+    query = CGI.escape('#onecushion')
+    
+    if Tweet.all.count > 0
+      outside_tweets = Twitter::Search.new(query).since(Tweet.last.id)
+    else
+      outside_tweets = Twitter::Search.new(query)
+    end
+    outside_tweets.each do |ot|
+      t = Tweet.new
+      t.attributes = ot
+      t.save
+    end
+  end
 end
