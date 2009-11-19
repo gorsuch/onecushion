63e7daa6627d004ac995da7cf3ea1c84f8d79062
Michael Gorsuch <michael@styledbits.com>
2009-11-19 17:33:29 -0500

    another layout tweek.  a little more inviting

--- cushion.rb
+++ cushion.rb
@@ -11,6 +11,6 @@ end
 get "/" do
   @tweets = Tweet.find(:all, :order => 'twitter_id DESC')
   @minutes = Tweet.sum("minutes")
-  @title = "One Cushion - #{@minutes} minutes of meditaiton logged."
+  @title = "One Cushion"
   erb :index
 end
